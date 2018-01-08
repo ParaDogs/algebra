c4b42d1fa02167597e27c1f638a436379ac6c473
ParaDogs <vakabakayo@yandex.ru>
2018-01-08 15:34:34 +1000

    Микро фикс

--- Ex/main.cpp
+++ Ex/main.cpp
@@ -12,6 +12,8 @@ int negative(int a){
     return (T - a) % T;
 }
 
+//bool checkPrime
+
 struct polynom{ //Полином, характеризующийся своей сопровождающей матрицей
     int deg;
     matrix* mat;
@@ -41,17 +43,25 @@ vector<matrix> expansion(field F, polynom P){ //Расширение поля F 
     vector<matrix> result;
     matrix E(P.deg, P.mat->mod);
     for(int i = 0; i < P.deg; i++) E.table[i][i] = 1;
-    for(int i = 0; i < (int)pow(F.deg, P.deg); i++){
-        result.push_back(E*(i%F.deg) + ((*(P.mat))*((int)(i/F.deg)%F.deg)) + (((*(P.mat))^2)*((int)(i/F.deg)%F.deg)));
-    }
+    for(int i = 0; i < (int)pow(F.deg, P.deg) + 1; i++)
+        result.push_back(E*(i%F.deg) + ((*(P.mat))*((int)(i/F.deg)%F.deg)) + (((*(P.mat))^2)*((int)(i/(F.deg*F.deg))%F.deg)));
+        //Тут происходит формирование поля расширения как всевозможных комбинаций корня и элементов поля
+        //В данном случае вот так: E + A + A^2 (каждое слагаемое с коэффициентом от 0 до 5. Итого 5^3 элементов в поле)
+        //Нужен(чтобы было прикольнее) более общий случай: E + A + A^2 + .. + A^(n-1), где n -- степень полинома
     return result;
 }
 
 
 int main(){
     field F5(N); //Изначальное поле
-    polynom A(D, 2, 0, 3); //Сопр. матрица неприводимого полинома 3 степени над F5
+    polynom A(D, 2, 0, 3); //Сопр. матрица неприводимого полинома 3 степени над F5. Сюда просто вписываем последние 3 коэф. вашего полинома
+    vector<matrix> exF5 = expansion(F5, A);
 
-    (*(A.mat)^124).print();
+    for(int i = 0; i < (int)pow(F5.deg, A.deg)+1; i++){
+        printf("%d %d %d\n", i%F5.deg, (int)(i/F5.deg)%F5.deg, (int)(i/(F5.deg*F5.deg))%F5.deg);
+        exF5[i].print();
+    }
+
+
     return 0;
 }
